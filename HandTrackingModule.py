8e3664bd2a64a6fecaa6bf087ba6b18c8c81f95e
Prakhar-Misra <60985897+Prakhar-Misra@users.noreply.github.com>
2021-09-13 10:25:24 +0530

    Update HandTrackingModule.py

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 from mediapipe.python.solutions import hands
+import math
 
 
 class HandTracker():
@@ -23,11 +24,11 @@ class HandTracker():
             for handLms in self.results.multi_hand_landmarks:
                 if draw:                   
                     self.mpDraw.draw_landmarks(img, handLms, self.mp_hands.HAND_CONNECTIONS)
-        
+
         return img
 
     def findPossition(self, img, handNo = 0, draw = True):
-        lmlist =[]
+        self.lmlist =[]
 
         if self.results.multi_hand_landmarks:
             myHand = self.results.multi_hand_landmarks[handNo]
@@ -36,9 +37,40 @@ class HandTracker():
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 #print(cx,cy)
-                lmlist.append([id, cx, cy])
+                self.lmlist.append([id, cx, cy])
+
+        return self.lmlist
+
+    
+    def FingersUp(self):
+        tipIds = [4,8,12,16,20]
+        fingers = []
+        if self.lmlist[tipIds[0]][1] > self.lmlist[tipIds[0]+1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
         
-        return lmlist
+        for id in range(1,5):
+            if self.lmlist[tipIds[id]][2] < self.lmlist[tipIds[id]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+        # totalFingers = sum(fingers)
+        return fingers
+    
+    def FindDistance(self, f1, f2, img, draw=False):
+        x1,y1 = self.lmlist[f1][1], self.lmlist[f1][2]
+        x2,y2 = self.lmlist[f2][1], self.lmlist[f2][2]
+        cx, cy = (x1+x2)//2, (y1+y2)//2
+
+        if draw:
+            cv2.circle(img, (x1,y1), 15, (255,0,255),cv2.FILLED)
+            cv2.circle(img, (x2,y2), 15, (255,0,255),cv2.FILLED)
+            cv2.line(img, (x1,y1), (x2,y2), (255, 0, 255), 3)
+            cv2.circle(img, (cx,cy), 15,(255,0,255), cv2.FILLED)
+
+        length = math.hypot(x2-x1, y2-y1)
+        return length, img, [x1,x2,y1,y2,cx,cy]
 def main():
     cap = cv2.VideoCapture(0)
 
@@ -50,6 +82,9 @@ def main():
         lmlist = detector.findPossition(img)
         #if len(lmlist) != 0:    
             #print(lmlist[])
+        if(len(lmlist)>0):
+            fingers = detector.FingersUp()
+            print(fingers)
 
         cv2.imshow('Image', img)
         cv2.waitKey(1)
@@ -57,4 +92,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main() 
